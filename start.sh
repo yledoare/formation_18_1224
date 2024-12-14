b69bcff90af1bd047081bbfaa86c54f1aa6d87c1
Yann Le Doaré <yann@linuxconsole.org>
2024-12-14 18:37:02 +0100

    fix patch

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 [ ! -e venv ] && python3 -m venv venv
 . venv/bin/activate
-[ ! -e odoo ] && git clone --depth 1 -b 18.0 https://github.com/odoo/odoo && [ -e /home/linuxconsole2024/x86_64/ ] && cd odoo && patch -p1 < linuxconsole-odoo.patch && cd ..
+[ ! -e odoo ] && git clone --depth 1 -b 18.0 https://github.com/odoo/odoo 
+[ -e /home/linuxconsole2024/x86_64/ ] && cd odoo && patch -p1 < ../linuxconsole-odoo.patch && cd ..
 pip3 install -r odoo/requirements.txt
 [ -e /home/linuxconsole2024/x86_64/lib/python3.10/site-packages/ ] && export PYTHONPATH=/home/linuxconsole2024/x86_64/lib/python3.10/site-packages/:$PYTHONPATH
 ./odoo/odoo-bin -d postgres  --db_host localhost -r odoo -w odoo -i base
