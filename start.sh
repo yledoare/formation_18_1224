bf1fdf9fda0d6622dff927dfd2327099200f2064
Yann Le Doaré <yann@linuxconsole.org>
2024-12-15 17:30:02 +0100

    Start with estate_property_management

--- start.sh
+++ start.sh
@@ -8,8 +8,8 @@ fi
 [ ! -e venv ] && python3 -m venv venv
 . venv/bin/activate
 [ ! -e odoo ] && git clone --depth 1 -b 18.0 https://github.com/odoo/odoo 
-[ -e /home/linuxconsole2024/x86_64/ ] && grep psycopg2 odoo/requirements.txt && cd odoo && patch -p1 < ../linuxconsole-odoo.patch && cd ..
+[ -e /home/linuxconsole2024/x86_64/ ] && grep psycopg2 odoo/requirements.txt && cd odoo && pwd && patch -p1 < ../linuxconsole-odoo.patch && cd ..
 pip3 install -r odoo/requirements.txt
 [ -e /home/linuxconsole2024/x86_64/lib/python3.10/site-packages/ ] && export PYTHONPATH=/home/linuxconsole2024/x86_64/lib/python3.10/site-packages/:$PYTHONPATH
-./odoo/odoo-bin -d postgres  --db_host localhost -r odoo -w odoo --addons-path=$PWD/addons,$PWD/odoo/addons # -i estate_property_management -u estate_property_management
+./odoo/odoo-bin -d postgres  --db_host localhost -r odoo -w odoo --addons-path=$PWD/addons,$PWD/odoo/addons -i estate_property_management -u estate_property_management
 #./odoo/odoo-bin -d postgres  --db_host localhost -r odoo -w odoo -i base  --addons-path=$PWD/addons,$PWD/odoo/addons # -i estate_property_management -u estate_property_management
