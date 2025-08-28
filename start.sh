580df047ff55e0fb53b1e7a75aa6fc8094b57b71
Yann Le Doaré <yann@linuxconsole.org>
2025-08-28 17:05:24 +0200

    psql

--- start.sh
+++ start.sh
@@ -9,8 +9,13 @@ fi
 [ ! -e venv ] && python3 -m venv venv
 . venv/bin/activate
 [ ! -e odoo ] && git clone --depth 1 -b 18.0 https://github.com/odoo/odoo 
-[ -e /home/linuxconsole2024/x86_64/ ] && grep psycopg2 odoo/requirements.txt && cd odoo && pwd && patch -p1 < ../linuxconsole-odoo.patch && cd ..
+if [ -e /home/linuxconsole2024/x86_64/ ] 
+then
+  cd odoo
+  grep psycopg2 requirements.txt && patch -p1 < ../linuxconsole-odoo.patch 
+  cd ..
+fi
 pip3 install -r odoo/requirements.txt
 [ -e /home/linuxconsole2024/x86_64/lib/python3.10/site-packages/ ] && export PYTHONPATH=/home/linuxconsole2024/x86_64/lib/python3.10/site-packages/:$PYTHONPATH
 ./odoo/odoo-bin -d postgres  --db_host localhost -r odoo -w odoo --addons-path=$PWD/addons,$PWD/odoo/addons -i estate_property_management -u estate_property_management
-#./odoo/odoo-bin -d postgres  --db_host localhost -r odoo -w odoo -i base  --addons-path=$PWD/addons,$PWD/odoo/addons # -i estate_property_management -u estate_property_management
+# docker exec -ti docker-db-1 psql -h localhost -U odoo postgres
